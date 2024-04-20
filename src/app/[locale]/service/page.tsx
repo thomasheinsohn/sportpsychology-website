e5f53f3696a52fd6a64b0d116402bb18772fb385
thomasheinsohn <94043568+thomasheinsohn@users.noreply.github.com>
2024-04-20 21:29:15 +0200

    Update page.tsx

--- src/app/[locale]/service/page.tsx
+++ src/app/[locale]/service/page.tsx
@@ -65,6 +65,8 @@ export default function Service() {
     <SimpleLayout
       title="Things I’ve made trying to put my dent in the universe."
       intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      subTitle=""
+      subContent=""
     >
       <ul
         role="list"
