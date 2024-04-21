68397e1ad226b349867e706ced4d244fcd72244d
thomasheinsohn <94043568+thomasheinsohn@users.noreply.github.com>
2024-04-21 14:30:03 +0200

    updated Readme and the .env file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -156,8 +156,9 @@ function NavItem({
 }) {
   let isActive
   const pathname = usePathname()
+  console.log(pathname, href)
 
-  if (href === '/') {
+  if (href === '/de') {
     isActive = pathname === href
   } else {
     isActive = usePathname().includes(href)
@@ -170,8 +171,8 @@ function NavItem({
         className={clsx(
           'relative block px-3 py-2 transition',
           isActive
-            ? 'text-teal-500 dark:text-teal-400'
-            : 'hover:text-teal-500 dark:hover:text-teal-400',
+            ? 'text-red-500 dark:text-red-400'
+            : 'hover:text-red-500 dark:hover:text-red-400',
         )}
       >
         {children}
@@ -225,9 +226,7 @@ function LanguageToggle() {
       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
       onClick={onChange}
     >
-      {/* <div className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600"> */}
       {language}
-      {/* </div> */}
     </button>
   )
 }
