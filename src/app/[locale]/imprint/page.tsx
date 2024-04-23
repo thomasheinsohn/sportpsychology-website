44154d7fff6dd0bd7e48273b460848e4b63bfd7d
thomasheinsohn <94043568+thomasheinsohn@users.noreply.github.com>
2024-04-23 16:01:39 +0200

    adjusted margins oft the pages

--- src/app/[locale]/imprint/page.tsx
+++ src/app/[locale]/imprint/page.tsx
@@ -7,8 +7,8 @@ export default function Imprint() {
   use(checkSession())
   const t = useTranslations('Imprint')
   return (
-    <Container className="mt-16 sm:mt-32">
-      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+    <Container className="mt-10 sm:mt-16">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
         <div className="lg:pl-20">
           <div className="max-w-xs px-2.5 lg:max-w-none"></div>
         </div>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -203,7 +203,7 @@ export default function Home() {
 
   return (
     <>
-      <Container className="mt-9">
+      <Container className="mt-12 sm:mt-16">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
             {t('title')}
