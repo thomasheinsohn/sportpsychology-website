4b4c3a1bc81953dfec94195098618d5bcbd8fcc6
thomasheinsohn <94043568+thomasheinsohn@users.noreply.github.com>
2024-05-09 16:26:31 +0200

    added picture and color

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,96 +1,61 @@
-import { Card } from '@/components/Card'
-import { Section } from '@/components/Section'
-import { SimpleLayout } from '@/components/SimpleLayout'
+import Image from 'next/image'
 import { use } from 'react'
-import { checkSession } from '../../../../lib'
+import { Container } from '@/components/Container'
+import portraitImage from '@/images/hanna-color.jpeg'
 import { useTranslations } from 'next-intl'
+import { checkSession } from '../../../../lib'
 
-function SpeakingSection({
-  children,
-  ...props
-}: React.ComponentPropsWithoutRef<typeof Section>) {
-  return (
-    <Section {...props}>
-      <div className="space-y-16">{children}</div>
-    </Section>
-  )
-}
-
-function Appearance({
-  title,
-  description,
-  event,
-  cta,
-  href,
-}: {
-  title: string
-  description: string
-  event: string
-  cta: string
-  href: string
-}) {
-  return (
-    <Card as="article">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
-      <Card.Description>{description}</Card.Description>
-      <Card.Cta>{cta}</Card.Cta>
-    </Card>
-  )
-}
 export default function About() {
   use(checkSession())
   const t = useTranslations('About')
   return (
-    <SimpleLayout
-      title={t('title')}
-      intro={t('content')}
-      subTitle={t('subTitle')}
-      subContent={t('subContent')}
-    >
-      <div className="space-y-20">
-        <SpeakingSection title={t('education')}>
-          <Appearance
-            href="#"
-            title="In space, no one can watch you stream — until now"
-            description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
-            event="SysConf 2021"
-            cta="Watch video"
-          />
-          <Appearance
-            href="#"
-            title="Lessons learned from our first product recall"
-            description="They say that if you’re not embarassed by your first version, you’re doing it wrong. Well when you’re selling DIY space shuttle kits it turns out it’s a bit more complicated."
-            event="Business of Startups 2020"
-            cta="Watch video"
-          />
-        </SpeakingSection>
-        <SpeakingSection title={t('licenses')}>
-          <Appearance
-            href="#"
-            title="Using design as a competitive advantage"
-            description="How we used world-class visual design to attract a great team, win over customers, and get more press for Planetaria."
-            event="Encoding Design, July 2022"
-            cta="Listen to podcast"
-          />
-          <Appearance
-            href="#"
-            title="Bootstrapping an aerospace company to $17M ARR"
-            description="The story of how we built one of the most promising space startups in the world without taking any capital from investors."
-            event="The Escape Velocity Show, March 2022"
-            cta="Listen to podcast"
-          />
-          <Appearance
-            href="#"
-            title="Programming your company operating system"
-            description="On the importance of creating systems and processes for running your business so that everyone on the team knows how to make the right decision no matter the situation."
-            event="How They Work Radio, September 2021"
-            cta="Listen to podcast"
-          />
-        </SpeakingSection>
+    <Container className="mt-10 sm:mt-16">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            {t('title')}
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+              I’ve loved making things for as long as I can remember, and wrote
+              my first program when I was 6 years old, just two weeks after my
+              mom brought home the brand new Macintosh LC 550 that I taught
+              myself to type on.
+            </p>
+            <p>
+              The only thing I loved more than computers as a kid was space.
+              When I was 8, I climbed the 40-foot oak tree at the back of our
+              yard while wearing my older sister’s motorcycle helmet, counted
+              down from three, and jumped — hoping the tree was tall enough that
+              with just a bit of momentum I’d be able to get to orbit.
+            </p>
+            <p>
+              I spent the next few summers indoors working on a rocket design,
+              while I recovered from the multiple surgeries it took to fix my
+              badly broken legs. It took nine iterations, but when I was 15 I
+              sent my dad’s Blackberry into orbit and was able to transmit a
+              photo back down to our family computer from space.
+            </p>
+            <p>
+              Today, I’m the founder of Planetaria, where we’re working on
+              civilian space suits and manned shuttle kits you can assemble at
+              home so that the next generation of kids really <em>can</em> make
+              it to orbit — from the comfort of their own backyards.
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">...</div>
       </div>
-    </SimpleLayout>
+    </Container>
   )
 }
