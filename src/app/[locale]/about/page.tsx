181f6456ee2a8f45a0d2e924ed0fc0137199d6bd
thomasheinsohn <tom.heinsohn98@icloud.com>
2024-05-13 17:46:39 +0200

    page.tsx aktualisieren

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -61,7 +61,7 @@ export default function About() {
               type="button"
               className="group mr-3 justify-around rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
             >
-              <NavLink href="/contact">Services</NavLink>
+              <NavLink href="/service">Services</NavLink>
             </button>
           </div>
           {/* <div className="mt-2">Kontaktiere mich:</div> */}
