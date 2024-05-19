5f00e43e1e18dbb1678727ee0b3c6e9f40480594
thomasheinsohn <94043568+thomasheinsohn@users.noreply.github.com>
2024-05-19 17:48:14 +0200

    next-sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,5 @@
 module.exports = {
-  siteUrl: 'https://www.movementpsychology.de', // Ersetze dies mit der URL deiner Webseite
-  generateRobotsTxt: true, // Erstellt auch eine robots.txt-Datei
-  sitemapSize: 5000, // Maximale Anzahl von URLs pro Sitemap-Datei
-  outDir: './public', // Der Ausgabeordner innerhalb deines Projekts
+  siteUrl: 'https://www.movementpsychology.de',
+  generateRobotsTxt: true,
+  outDir: './public',
 }
